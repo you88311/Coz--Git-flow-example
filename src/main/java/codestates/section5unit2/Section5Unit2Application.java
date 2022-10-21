dcc9c141366c6a7f6d45bac3dd581505c4f7770d
you88311@naver.com <you88311@naver.com>
2022-10-21 15:23:10 +0900

    simpleController 추가

---
+++ src/main/java/codestates/section5unit2/Section5Unit2Application.java
@@ -0,0 +1,13 @@
+package codestates.section5unit2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section5Unit2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section5Unit2Application.class, args);
+	}
+
+}
---
+++ src/test/java/codestates/section5unit2/Section5Unit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package codestates.section5unit2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section5Unit2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
